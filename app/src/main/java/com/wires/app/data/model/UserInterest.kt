13db5823beea24d38e338d2a94ca5d41c39e3be3
Konstantin Goncharov <kostjag15@gmail.com>
2022-05-11 18:16:46 +0300

    Add interests

--- app/src/main/java/com/wires/app/data/model/UserInterest.kt
+++ app/src/main/java/com/wires/app/data/model/UserInterest.kt
@@ -1,7 +1,10 @@
 package com.wires.app.data.model
 
 enum class UserInterest(val value: String) {
-    ANDROID_DEVELOPMENT("Android Dev"),
-    IOS_DEVELOPMENT("IOS Dev"),
-    WEB_DEVELOPMENT("Web Dev")
+    ANDROID_DEVELOPMENT("Android"),
+    IOS_DEVELOPMENT("IOS"),
+    WEB_DEVELOPMENT("Web"),
+    QUALITY_ASSISTANCE("QA"),
+    DESIGN("UI/UX"),
+    DATA_SCIENCE("Data Science")
 }
