aa8111621f29c5f97903700d4885dfa45e035d82
Konstantin Goncharov <kostjag15@gmail.com>
2022-06-02 00:50:27 +0300

    Add password toggle button to fields

--- app/src/main/java/com/wires/app/presentation/views/ValidationTextInput.kt
+++ app/src/main/java/com/wires/app/presentation/views/ValidationTextInput.kt
@@ -1,15 +1,23 @@
 package com.wires.app.presentation.views
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.FrameLayout
 import androidx.core.content.withStyledAttributes
 import com.wires.app.R
 import com.wires.app.databinding.ViewTextInputBinding
+import com.wires.app.extensions.getDrawableCompat
+import com.wires.app.extensions.orDefault
 
 class ValidationTextInput @JvmOverloads constructor(
     context: Context,
@@ -21,6 +29,7 @@ class ValidationTextInput @JvmOverloads constructor(
         private const val DEFAULT_MIN_TEXT_LENGTH = 3
         private const val DEFAULT_MAX_TET_LENGTH = 20
         private const val DEFAULT_REGEX = "^[a-zA-Z0-9]*$"
+        private const val EDIT_TEXT_END_ICON_INDEX = 2
     }
 
     private val binding = ViewTextInputBinding.inflate(LayoutInflater.from(context), this, true)
@@ -43,9 +52,9 @@ class ValidationTextInput @JvmOverloads constructor(
         }
 
     var text: String?
-        get() = binding.editText.text.toString()
+        get() = editText.text.toString()
         set(value) {
-            binding.editText.setText(value)
+            editText.setText(value)
         }
 
     val editText = binding.editText
@@ -57,48 +66,15 @@ class ValidationTextInput @JvmOverloads constructor(
                 R.styleable.ValidationTextInput,
                 defStyleAttr
             ) {
-                lengthError = getString(R.styleable.ValidationTextInput_lengthErrorText)
-                patternError = getString(R.styleable.ValidationTextInput_patternErrorText)
-                additionalValidationError = getString(R.styleable.ValidationTextInput_additionalValidationErrorText)
-                minTextLength = getInt(R.styleable.ValidationTextInput_minTextLength, DEFAULT_MIN_TEXT_LENGTH)
-                maxTextLength = getInt(R.styleable.ValidationTextInput_maxTextLength, DEFAULT_MAX_TET_LENGTH)
-                validationRegex =
-                    getString(R.styleable.ValidationTextInput_validationPattern)?.toRegex() ?: DEFAULT_REGEX.toRegex()
-                canBeEmpty = getBoolean(R.styleable.ValidationTextInput_canBeEmpty, false)
-                with(editText) {
-                    setCompoundDrawablesWithIntrinsicBounds(
-                        getDrawable(R.styleable.ValidationTextInput_fieldStartIcon),
-                        null,
-                        getDrawable(R.styleable.ValidationTextInput_fieldEndIcon),
-                        null
-                    )
-                    hint = getString(R.styleable.ValidationTextInput_fieldHint)
-                    imeOptions = if (getInt(R.styleable.ValidationTextInput_fieldImeOptions, 0) == 0) {
-                        EditorInfo.IME_ACTION_NEXT
-                    } else {
-                        EditorInfo.IME_ACTION_DONE
-                    }
-                    when (getInt(R.styleable.ValidationTextInput_fieldInputType, 0)) {
-                        1 -> inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
-                        2 -> {
-                            inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-                            typeface = Typeface.DEFAULT
-                        }
-                        else -> inputType = InputType.TYPE_CLASS_TEXT
-                    }
-                    setOnFocusChangeListener { _, hasFocus ->
-                        if (!hasFocus) {
-                            validate()
-                            onFocusRemoved()
-                        }
-                    }
-                }
+                setCommonAttrs(this)
+                setEditTextAttrs(this)
+                bindIcons(this)
             }
         }
     }
 
     fun validate(): Boolean {
-        val text = binding.editText.text.toString()
+        val text = editText.text.toString()
         error = when {
             text.isBlank() -> if (canBeEmpty) null else emptyFieldError
             text.length !in minTextLength..maxTextLength -> lengthError
@@ -112,6 +88,77 @@ class ValidationTextInput @JvmOverloads constructor(
     fun isEmpty() = text.isNullOrEmpty()
 
     fun setOnEditorActionListener(action: () -> Boolean) {
-        binding.editText.setOnEditorActionListener { _, _, _ -> action() }
+        editText.setOnEditorActionListener { _, _, _ -> action() }
+    }
+
+    private fun setCommonAttrs(attrs: TypedArray) = with(attrs) {
+        lengthError = getString(R.styleable.ValidationTextInput_lengthErrorText)
+        patternError = getString(R.styleable.ValidationTextInput_patternErrorText)
+        additionalValidationError = getString(R.styleable.ValidationTextInput_additionalValidationErrorText)
+        minTextLength = getInt(R.styleable.ValidationTextInput_minTextLength, DEFAULT_MIN_TEXT_LENGTH)
+        maxTextLength = getInt(R.styleable.ValidationTextInput_maxTextLength, DEFAULT_MAX_TET_LENGTH)
+        validationRegex =
+            getString(R.styleable.ValidationTextInput_validationPattern)?.toRegex() ?: DEFAULT_REGEX.toRegex()
+        canBeEmpty = getBoolean(R.styleable.ValidationTextInput_canBeEmpty, false)
+    }
+
+    private fun setEditTextAttrs(attrs: TypedArray) = with(attrs) {
+        with(editText) {
+            hint = getString(R.styleable.ValidationTextInput_fieldHint)
+            imeOptions = if (getInt(R.styleable.ValidationTextInput_fieldImeOptions, 0) == 0) {
+                EditorInfo.IME_ACTION_NEXT
+            } else {
+                EditorInfo.IME_ACTION_DONE
+            }
+            when (getInt(R.styleable.ValidationTextInput_fieldInputType, 0)) {
+                1 -> inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+                2 -> {
+                    inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                    typeface = Typeface.DEFAULT
+                }
+                else -> inputType = InputType.TYPE_CLASS_TEXT
+            }
+            setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    validate()
+                    onFocusRemoved()
+                }
+            }
+        }
+    }
+
+    private fun bindIcons(attrs: TypedArray) = with(attrs) {
+        var endIconRes: Drawable? = null
+        if (getBoolean(R.styleable.ValidationTextInput_fieldPasswordToggleEnabled, false)) {
+            endIconRes = context.getDrawableCompat(R.drawable.ic_password_toggle)
+            setPasswordToggleClickListener()
+        }
+        editText.setCompoundDrawablesWithIntrinsicBounds(
+            getDrawable(R.styleable.ValidationTextInput_fieldStartIcon),
+            null,
+            endIconRes,
+            null
+        )
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setPasswordToggleClickListener() = with(editText) {
+        setOnTouchListener { view, motionEvent ->
+            if (motionEvent.rawX >= (right - compoundDrawables[EDIT_TEXT_END_ICON_INDEX].bounds.width())) {
+                if (motionEvent.action == MotionEvent.ACTION_UP) {
+                    transformationMethod =
+                        if (transformationMethod != PasswordTransformationMethod.getInstance()) {
+                            PasswordTransformationMethod.getInstance()
+                        } else {
+                            HideReturnsTransformationMethod.getInstance()
+                        }
+                    setSelection(text?.length.orDefault())
+                    view.isSelected = !view.isSelected
+                }
+                true
+            } else {
+                false
+            }
+        }
     }
 }
