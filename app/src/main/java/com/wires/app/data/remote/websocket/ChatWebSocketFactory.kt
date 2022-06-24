bfedb4c8dbea25c5e6d2ee86801c06e7fbbc94e7
Konstantin Goncharov <kostjag15@gmail.com>
2022-06-25 00:41:48 +0300

    Fix socket headers

--- app/src/main/java/com/wires/app/data/remote/websocket/ChatWebSocketFactory.kt
+++ app/src/main/java/com/wires/app/data/remote/websocket/ChatWebSocketFactory.kt
@@ -12,11 +12,10 @@ class ChatWebSocketFactory @Inject constructor(
     companion object {
         private const val CHAT_WEBSOCKET_URL = "wss://wires-api.herokuapp.com/v1/channels/%d/listen"
         private const val AUTH_HEADER_NAME = "Authorization"
-        private const val AUTH_TOKEN_PREFIX = "Bearer"
     }
 
     fun create(channelId: Int): WebSocket =
         webSocketFactory.createSocket(CHAT_WEBSOCKET_URL.format(channelId)).apply {
-            addHeader(AUTH_HEADER_NAME, "$AUTH_TOKEN_PREFIX ${getAccessTokenUseCase.execute(Unit)}")
+            addHeader(AUTH_HEADER_NAME, getAccessTokenUseCase.execute(Unit))
         }
 }
